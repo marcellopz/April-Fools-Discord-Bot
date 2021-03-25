12b07eb93eb7c8910cbd901727a23a7ee4581184
Zan <marcellopz@hotmail.com>
2021-03-25 14:00:37 -0300

    unfinished

--- bot.py
+++ bot.py
@@ -14,15 +14,19 @@ load_dotenv()
 BOT_KEY = os.getenv("BOT_KEY")
 logging.basicConfig()
 logger = logging.getLogger('discord')
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 client = commands.Bot(command_prefix = '.', intents = intents)
 
+event_channel = 727673871133835275
+botroom = 316142433828208643
+emoji_id = 817067077591629875
 event_role_id = 820349883504525362
 currently_playing = []
 bets = []
+allowed_channels = [botroom, event_channel]
 
 @client.event
 async def on_ready():
@@ -34,6 +38,11 @@ async def on_message(message):
     if message.author.bot:
         return
     await client.process_commands(message)
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.emoji.id == emoji_id:
+        await payload.member.add_roles((await client.fetch_guild(payload.guild_id)).get_role(event_role_id))
 
 
 @client.command()
@@ -51,6 +60,8 @@ async def bet(ctx, *, args: str):
         team_name = args[slice(-1*len(value) - 1)].lower()
         try:
             value = int(value)
+            if value<1:
+                raise
             if team_name not in currently_playing:
                 raise
             with open('members/' + str(ctx.author.id) + '.json', 'r') as read_file:
@@ -86,6 +97,20 @@ async def current_bets(ctx):
         for bet in bets:
             answer = answer + '- **{}** is betting **{}** on **{}** with a rate of **{}**\n'.format(bet['name'], bet['value'], bet['team'], bet['rate'])
         await ctx.send(answer)
+    else:
+        await ctx.send("No bets are placed currently")
+
+@client.command()
+async def mybets(ctx):
+    global bets
+    if bets:
+        answer = 'Your bet(s) are:\n'
+        for bet in bets:
+            if bet['id']==ctx.author.id:
+                answer = answer + "**{}** on **{}** with a rate of **{}**\n".format(bet['value'], bet['team'], bet['rate'])
+        await ctx.send(answer)
+    else:
+        await ctx.send("You have no bets placed currently")
 
 @client.command()
 async def leaderboard(ctx):
@@ -98,11 +123,15 @@ async def leaderboard(ctx):
     resp = "The Leaderboard is:\n\n"
     for member in enumerate(sorted_list):
         resp += "**{}**. {} - {}\n".format(member[0]+1, member[1]['name'], member[1]['coins'])
+        if member[0] == 9:
+            break
     await ctx.send(resp)
 
 
 @client.command()
 async def join(ctx):
+    if ctx.channel.id not in allowed_channels:
+        return
     if os.path.isfile('members/' + str(ctx.author.id) + '.json'):
         await ctx.send("You're already joined the event")
     else:
@@ -129,12 +158,18 @@ async def balance(ctx):
         
 @client.command()
 async def match(ctx, role1: discord.Role, role2: discord.Role):
+    if ctx.author.id not in mod_team:
+        return
     global currently_playing
+    global bets
     currently_playing = [role1.name.lower(), role2.name.lower()]
+    print(currently_playing)
     winner = await event.event(role1, role2, ctx)
+    await ctx.send(f'{winner.mention} wins!')
     if bets:
         for bet in bets:
-            if bet['team'] == winner.name:
+            if bet['team'] == winner.name.lower():
+                logging.debug(bet)
                 print(bet)
                 with open('members/' + str(bet['id']) + '.json', 'r') as read_file:
                     member = json.load(read_file)
@@ -142,5 +177,8 @@ async def match(ctx, role1: discord.Role, role2: discord.Role):
                 with open('members/' + str(bet['id']) + '.json', 'w') as write_file:
                     json.dump(member, write_file, indent=4)
     currently_playing = []
+    bets = []
+    event.stimulusCheck()
+    
 
 client.run(BOT_KEY)
--- event.py
+++ event.py
@@ -2,22 +2,30 @@ import time
 import random
 import asyncio
 import math
+import discord
+import os
+import json
 
 global rates
 
 async def event(team1, team2, ctx):
     global rates
     rates = {
-        team1.name: 2,
-        team2.name: 2
+        team1.name.lower(): 2,
+        team2.name.lower(): 2
     }
-    rounds = 7 # number of rounds
+    rounds = random.choice([5, 6, 7, 8]) # number of rounds
+    print(rounds)
     dice = 3 # d4 rolls
     team1_score = [0] # array of scores
     team2_score = [0]
     lead = [] # array of who was in the lead
-    time_interval = 0.2 # minutes
-    await ctx.send(f'Match between {team1.mention} and {team2.mention} is starting in {time_interval} minutes! Updates also every {time_interval} minutes.\nGet your bets ready!')
+    time_interval = 0.1 # minutes
+    embed0 = discord.Embed(color=0xff4a4a)
+    embed0.add_field(name="Match starting!", value=f'Match between {team1.mention} and {team2.mention} is starting in {time_interval} minutes! \nUpdates also every {time_interval} minutes.\n\nGet your bets ready!', inline=False)
+    embed0.add_field(name=f"{team1.name} rates:", value=2.0, inline=True)
+    embed0.add_field(name=f"{team2.name} rates:", value=2.0, inline=True)
+    await ctx.send(embed=embed0)
     
     for i in range(rounds): # game
         await asyncio.sleep(60 * time_interval)
@@ -29,59 +37,40 @@ async def event(team1, team2, ctx):
                 team1_score[i] += 1
         team1_score.append(team1_score[i] + random.randint(1, dice))
         team2_score.append(team2_score[i] + random.randint(1, dice))
-        [winner, loser, msg] = message(team1, team2, team1_score[i+1], team2_score[i+1], i+1, rounds)
-        if i is not rounds:
-            if lead:
-                if winner == 'tie':
-                    if lead[-1] == 'tie':
-                        msg += f'\n\nThe game is still even!'
-                    else:
-                        msg += f'\n\n**{team1.name if lead[-1] == team2.name else team2.name}** is back in the game!'
-                elif winner == lead[-1]:
-                    msg += f'\n**{winner}** is still in the lead!'
-                elif lead[-1] == 'tie':
-                    msg += f'\n**{winner}** takes the lead!'
-                else:
-                    msg += f'\n**{winner}** manages to turn it around!'
-            else:
-                if winner == 'tie':
-                    msg += f'\nThe match starts off even!'
-                else:
-                    msg += f'\n**{winner}** starts off with a lead!'
+        [winner, loser, msg] = message(team1, team2, team1_score[i+1], team2_score[i+1],team1_score[i], team2_score[i], i+1, rounds)
+        if i+1 is not rounds:
+            msg = get_state_message(winner, loser, lead, team1.name, team2.name, msg)
         lead.append(winner)
-        await ctx.send('‎ ‎  ‎ ‎ ‎ ‎\n\n➡️ ' + msg)
         
+        embed1=discord.Embed(color=0x9ca2e2)
+        embed1.add_field(name="Match update", value=msg, inline=False)
+        await ctx.send(embed=embed1)
+
         [team1_rates, team2_rates] = calculate_rates(dice, rounds - i - 1, team1_score[-1], team2_score[-1])
-        
         rates = {
-            team1.name: team1_rates,
-            team2.name: team2_rates
+            team1.name.lower(): team1_rates,
+            team2.name.lower(): team2_rates
         }
         print(rates)
+
+        embed2=discord.Embed(color=0x99f788)
         if team1_rates and team2_rates:
-            await ctx.send(f'Place your bets! The rates are:\n**{team1.name}**: {round(team1_rates,2)}\n**{team2.name}**: {round(team2_rates,2)}')
+            embed2.add_field(name=f"{team1.name} rates:", value=team1_rates, inline=True)
+            embed2.add_field(name=f"{team2.name} rates:", value=team2_rates, inline=True)
         else:
-            await ctx.send('Bets are closed!')
+            embed2.add_field(name="Bets are closed!",value="‎ ‎  ‎ ‎ ‎ ‎")
         print(team1_score, team2_score)
-        
+        await ctx.send(embed=embed2)
 
     # game end
-    if team1_score[-1] == team2_score[-1]:
-        winner = random.choice([team1, team2])
-        await ctx.send(f'Very close game, but in the end {winner} takes the victory!')
-    elif team1_score[-1]>team2_score[-1]:
-        await ctx.send(f'**{team1}** wins!')
+    if winner == team1.name:
         return team1
-    elif team2_score[-1]>team1_score[-1]:
-        await ctx.send(f'**{team2}** wins!')
+    else:
         return team2
-    
 
 
-
-
-def message(team1, team2, t1, t2, n, max_round): # decide what message to send
-    tie = True if t1==t2 else False
+def message(team1, team2, t1, t2, previousT1, previousT2, n, max_round): # decide what message to send
+    tie = True if t1==t2 and previousT1 == previousT2 else False
     if n<=0.3*max_round: # first 30% of the messages are about early game
         if n == 0:
             stage = random.choices(['early', 'firstblood'])
@@ -89,31 +78,95 @@ def message(team1, team2, t1, t2, n, max_round): # decide what message to send
             stage = 'early'
     elif n<=0.8*max_round: # messages between 30 and 80% are about mid game
         stage = 'mid'
+    elif n == max_round: # end game
+        stage = 'game_ending'
     else: # last 20% about late game
         stage = 'late'
-    
-    if tie:
+    print(stage)
+    if stage == 'game_ending':
+        if t1 == t2:
+            winner = random.choice([team1, team2])
+            loser = team1 if winner == team2 else team2
+        elif t1>t2:
+            winner = team1
+            loser = team2
+        else:
+            winner = team2
+            loser = team1
+        return([winner.name, loser.name,
+            get_template(stage).format(
+                winning_team = "**" + winner.name + "**",
+                losing_team = "**" + loser.name + "**",
+                winning_user = "**" + random.choice(winner.members).name + "**",
+                losing_user = "**" + random.choice(loser.members).name + "**"
+                )
+            ])   
+    elif tie:
         teams = [team1, team2]
         random.shuffle(teams)
-        return(['tie', 'tie', get_template(stage + '_tie').format(teamone = teams[0], teamtwo = teams[1])])
+        return(['tie', 'tie', 
+            get_template(stage + '_tie').format(
+                teamone = "**" + teams[0].name + "**",
+                teamtwo = "**" + teams[1].name + "**",
+                teamone_member = "**" + random.choice(teams[0].members).name + "**",
+                teamtwo_member = "**" + random.choice(teams[1].members).name + "**"
+                )
+            ])
     else:
+        oldDiff = previousT1 - previousT2
+        newDiff = t1 - t2
+        if oldDiff - newDiff == 0:
+            if t1>t2:
+                event_winner = team1
+                event_loser = team2
+            else:
+                event_winner = team2
+                event_loser = team1
+        elif oldDiff - newDiff > 0:
+            event_winner = team2
+            event_loser = team1
+        elif oldDiff - newDiff < 0:
+            event_winner = team1
+            event_loser = team2
+        #####################################################
         if t1>t2:
             winner = team1
             loser = team2
         else:
             winner = team2
             loser = team1
-        
+        #####################################################
         return([winner.name, loser.name,
             get_template(stage).format(
-                winning_team = winner.name,
-                losing_team = loser.name,
-                winning_user = random.choice(winner.members).name,
-                losing_user = random.choice(loser.members).name
+                winning_team = "**" + event_winner.name + "**",
+                losing_team = "**" + event_loser.name + "**",
+                winning_user = "**" + random.choice(event_winner.members).name + "**",
+                losing_user = "**" + random.choice(event_loser.members).name + "**"
                 )
-            ])
+            ])        
 
-        
+
+def get_state_message(winner, loser, lead, team1, team2, msg):
+    if lead:
+        if winner == 'tie':
+            if lead[-1] == 'tie':
+                msg += f'\n\nThe game is still even!'
+            else:
+                msg += f'\n\n**{team1 if lead[-1] == team2 else team2}** is back in the game!'
+        elif winner == lead[-1]:
+            msg += f'\n**{winner}** is still in the lead!'
+        elif lead[-1] == 'tie':
+            msg += f'\n**{winner}** takes the lead!'
+        else:
+            msg += f'\n**{winner}** manages to turn it around!'
+    else:
+        if winner == 'tie':
+            msg += f'\nThe match starts off even!'
+        else:
+            msg += f'\n**{winner}** starts off with a lead!'
+    return msg
+
+
 def get_template(file_name): # getting phrase from file
     file = open('templates/'+file_name+'.txt','r')
     templates = file.readlines()
@@ -153,4 +206,11 @@ def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
-#message(6,5,0)
+def stimulusCheck():
+    for filename in os.listdir('members/'):
+        with open('members/' + filename, 'r') as read_file:
+            member = json.load(read_file)
+        if member['coins'] < 10:
+            member['coins'] = 100
+            with open('members/' + filename, 'w') as write_file:
+                json.dump(member, write_file, indent=4)
